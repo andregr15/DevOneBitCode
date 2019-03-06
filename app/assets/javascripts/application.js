e65815d5c3341e29202f4a6d21389a1ff9a1de79
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-03-06 09:23:01 -0300

    Adding materialize, material-icons, fontawesome and jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
+//= require materialize-css/dist/js/materialize
 //= require activestorage
 //= require turbolinks
 //= require_tree .
