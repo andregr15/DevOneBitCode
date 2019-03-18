099d90b38b1d78bad5af20b61e95ed1f07d2f68c
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-03-18 16:51:26 -0300

    Adding tags_controller and portfolio_tags_controller

---
+++ app/policies/portfolio_tags_policy.rb
@@ -0,0 +1,15 @@
+class PortfolioTagsPolicy < ApplicationPolicy
+  def create?
+    permitted_access?
+  end
+
+  def destroy?
+    permitted_access?
+  end
+
+  private
+
+  def permitted_access?
+    record.portfolio.user == user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :portfolios, except: :new
+  resources :portfolios, except: :new do
+    resources :tags, only: %i[create destroy], param: :tag_id, controller: :portfolio_tags
+  end
+
+  resources :tags, only: :index
 end
