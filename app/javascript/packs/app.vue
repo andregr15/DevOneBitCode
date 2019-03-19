85efb162d7dcd7b1d53a543e4b2bac5d10965dff
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-03-19 06:37:36 -0300

    Adding block component

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,16 +1,19 @@
 <template>
   <portfolio></portfolio>
+  <block></block>
 </template>
 
 <script>
   import Portfolio from './components/portfolio';
+  import Block from './components/block';
 
   export default {
     data() {
       return { }
     },
     components: {
-      Portfolio
+      Portfolio,
+      Block
     }
   }
 </script>
