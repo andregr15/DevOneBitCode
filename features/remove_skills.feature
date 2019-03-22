c07afd0e362b704b6af8f0b8f508845ee8c938f3
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2019-03-22 11:47:06 -0300

    Adding skills tests

---
+++ features/remove_skills.feature
@@ -0,0 +1,15 @@
+# language:pt
+
+@javascript
+Funcionalidade: Remover habilidades do Portfólio
+  Remove uma habilidade do Portfólio
+
+  Cenário de Fundo:
+    Dado usuário está logado
+    E possui o portfólio com slug 'my_portfolio'
+    E o portfólio possui 1 habilidade
+    E acessa a página de edição do portfólio
+
+  Cenário: Usuário remove uma hablidade do portfólio
+    Quando remove a habilidade
+    Então a habilidade é removida do portfólio
---
+++ features/step_definitions/skills_steps.rb
@@ -0,0 +1,33 @@
+Dado "o portfólio possui um bloco de habilidades" do
+  create(:block, kind: :skill, side: :right, portfolio_id: @portfolio.id)
+end
+
+Dado "o portfólio possui 1 habilidade" do
+  create(:block, kind: :skill, side: :right, portfolio_id: @portfolio.id)
+  @skill = create(:skill, portfolio_id: @portfolio.id)
+end
+
+Quando "adiciona uma habilidade" do
+  @skill = attributes_for(:skill)
+  fill_in "skill-title", with: @skill[:title]
+  fill_in "skill-percent", with: @skill[:percent]
+  find("#add-skill").click
+  wait_for_ajax
+end
+
+Quando "remove a habilidade" do
+  find(".remove-skill").click
+  wait_for_ajax
+end
+
+Então "a habilidade é adicionada ao portfólio" do
+  @portfolio.reload
+  expect(@portfolio.skill.count).to eq(1)
+  expect(page).to have_css(".resource-skill", count: 1)
+end
+
+Então "a habilidade é removida do portfólio" do
+  @portfolio.reload
+  expect(@portfolio.skill.count).to eq(0)
+  expect(page).not_to have_css(".resource-skill")
+end
